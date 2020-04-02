de6794a7e3b0f37345d5efede3e19a2de05b3635
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-02 15:44:53 +0530

    third commit

---
+++ OurProducts/OurProducts/Models/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurProducts.Models
+{
+    public class ProductContext:DbContext
+
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<OurProducts.Models.Product> product { get; set; }
+    }
+
+}
--- OurProducts/OurProducts/Startup.cs
+++ OurProducts/OurProducts/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OurProducts.Models;
 
 namespace OurProducts
 {
@@ -24,6 +26,10 @@ namespace OurProducts
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProductContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
