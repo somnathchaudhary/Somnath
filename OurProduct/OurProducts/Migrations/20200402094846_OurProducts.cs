96711a60b758fc807d685fc7d816594e3b764dc4
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-02 15:33:21 +0530

    first commit

---
+++ OurProduct/OurProducts/Migrations/20200402094846_OurProducts.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OurProducts.Migrations
+{
+    public partial class OurProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    PId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PName = table.Column<string>(nullable: true),
+                    PDesc = table.Column<string>(nullable: true),
+                    PPrice = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.PId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
