a452524144aa8e2ca6670f8e01ef2f3976b99ec7
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-02 14:57:46 +0530

    Second Commit

---
+++ OurProducts/OurProducts/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurProducts.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string PDesc { get; set; }
+        public float PPrice { get; set; }
+    }
+}
