0ab1c0e0e9d39a871a218eed73ff6791c2d9a88f
somnathchaudhary <somuchaudhary0506@gmail.com>
2020-04-03 11:30:15 +0530

    Second commit

--- StudentController/StudentController/Controllers/StudentController.cs
+++ StudentController/StudentController/Controllers/StudentController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StudentController.Models;
 
 namespace StudentController.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Student()
         {
-            return View();
+            List<Student> stdlist = new List<Student>();
+            Student user = new Student();
+            user.Id = 01; user.Name = "Som"; user.fee = 1000;
+            return View(user);
+            
         }
     }
 }
